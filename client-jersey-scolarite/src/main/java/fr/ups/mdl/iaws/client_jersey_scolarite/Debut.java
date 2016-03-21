593b73e9c53ebb59825e8f1b411e4784da569e6c
Benjamin Saint-Sever <saintseverbenjamin@gmail.com>
2016-03-21 16:07:18 +0100

    Ajout du TP 3

---
+++ client-jersey-scolarite/src/main/java/fr/ups/mdl/iaws/client_jersey_scolarite/Debut.java
@@ -0,0 +1,12 @@
+package fr.ups.mdl.iaws.client_jersey_scolarite;
+
+public class Debut {
+
+	public static void main(String[] args) {
+			api_etudiant api = new api_etudiant();
+			api.chercher(21);
+		
+
+	}
+
+}
